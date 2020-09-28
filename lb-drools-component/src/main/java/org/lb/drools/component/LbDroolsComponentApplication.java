fa4d55cdb45d167a4cb1e58d2a0fb2df2bf7e9ac
kongkai <929145946@qq.com>
2020-09-28 09:56:10 +0800

    zxl:集成drools

---
+++ lb-drools-component/src/main/java/org/lb/drools/component/LbDroolsComponentApplication.java
@@ -0,0 +1,13 @@
+package org.lb.drools.component;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbDroolsComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LbDroolsComponentApplication.class, args);
+	}
+
+}
